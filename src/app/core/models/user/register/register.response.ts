e0c437e69b3b442300cdaffca30b8768738449c0
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-02 11:48:04 +0100

    feat: added authentication and email verification models

---
+++ src/app/core/models/user/register/register.response.ts
@@ -0,0 +1,9 @@
+export interface RegisterResponse {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    roles: string[];
+    createdAt: string;
+    updatedAt: string;
+}
