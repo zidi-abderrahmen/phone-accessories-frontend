ba034fabaa50bc20266e90813165f2a8a74dd502
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-03 00:43:08 +0100

    feat: implemented verify email component with state handling and error management

---
+++ src/app/components/verifyEmail/verify.email.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VerifyEmail } from './verify.email';
+
+describe('VerifyEmail', () => {
+  let component: VerifyEmail;
+  let fixture: ComponentFixture<VerifyEmail>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [VerifyEmail],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(VerifyEmail);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
