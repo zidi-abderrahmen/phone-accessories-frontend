77b85168496ac90aac8cdee842a597dae1843bb3
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-02 23:16:38 +0100

    feat: added simple brand component with HTML and SCSS

---
+++ src/app/shared/components/brand/brand.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brand',
+  imports: [],
+  templateUrl: './brand.html',
+  styleUrl: './brand.scss',
+})
+export class Brand {}
