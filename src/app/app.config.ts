0d63e5cb1eacc21599db8a27e057db9799f03618
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-02 15:25:33 +0100

    feat: enhanced app configuration with HTTP client and authentication initializer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { firstValueFrom } from "rxjs";
+import { routes } from "./app.routes";
+import { credentialsInterceptor } from "./core/interceptors/cookies/credentials-interceptor";
+import { errorInterceptor } from "./core/interceptors/error/error-interceptor";
+import { AuthService } from "./core/services/auth/auth.service";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([credentialsInterceptor, errorInterceptor])),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAppInitializer(() => {
+      const authService = inject(AuthService);
+      return firstValueFrom(authService.checkAuth());
+    })
   ]
 };
