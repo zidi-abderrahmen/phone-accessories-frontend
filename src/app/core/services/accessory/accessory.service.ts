f2dcb556d198f937db1a28b43bd7f9d5cbba0ab3
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-12 23:45:43 +0100

    feat: added searchAccessories() method for searching with filter

--- src/app/core/services/accessory/accessory.service.ts
+++ src/app/core/services/accessory/accessory.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { AccessoryResponse } from '../../models/accessory/accessory-response';
 import { Page } from '../../models/page';
 import { AccessoryRequest } from '../../models/accessory/accessory-request';
+import { SearchRequest } from '../../models/accessory/search/search-request';
 
 @Service()
 export class AccessoryService {
@@ -39,5 +40,38 @@ export class AccessoryService {
 
     deleteAccessory(id: number): Observable<void> {
         return this.http.delete<void>(`${this.apiUrl}/${id}`, { withCredentials: true });
+    }
+
+    searchAccessories(
+        data: SearchRequest,
+        page: number = 0,
+        size: number = 10,
+        sort?: string
+        ): Observable<Page<AccessoryResponse>> {
+        let params = new HttpParams()
+            .set('page', page.toString())
+            .set('size', size.toString());
+
+        if (sort) {
+            params = params.set('sort', sort);
+        }
+
+        if (data.categoryId != null) {
+            params = params.set('categoryId', data.categoryId.toString());
+        }
+        if (data.keyword) {
+            params = params.set('keyword', data.keyword);
+        }
+        if (data.minPrice != null) {
+            params = params.set('minPrice', data.minPrice.toString());
+        }
+        if (data.maxPrice != null) {
+            params = params.set('maxPrice', data.maxPrice.toString());
+        }
+        if (data.inStock != null) {
+            params = params.set('inStock', data.inStock.toString());
+        }
+
+        return this.http.get<Page<AccessoryResponse>>(`${this.apiUrl}/search`, { params });
     }
 }
