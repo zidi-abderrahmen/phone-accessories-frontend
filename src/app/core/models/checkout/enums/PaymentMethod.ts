f95f9893aa479a7dd9f003a3e63f0c83cb0048a7
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-25 21:28:04 +0100

    feat: added enums for OrderStatus, PaymentMethod, and ShippingMethod

---
+++ src/app/core/models/checkout/enums/PaymentMethod.ts
@@ -0,0 +1,5 @@
+export enum PaymentMethod {
+    CASH_ON_DELIVERY = 'CASH_ON_DELIVERY',
+    CREDIT_CARD = 'CREDIT_CARD',
+    PAYPAL = 'PAYPAL'
+}
---
+++ src/app/core/models/checkout/enums/ShippingMethod.ts
@@ -0,0 +1,4 @@
+export enum ShippingMethod {
+    STANDARD = 'STANDARD',
+    EXPRESS = 'EXPRESS'
+}
