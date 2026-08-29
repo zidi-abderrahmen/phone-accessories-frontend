30116792ea7d8853bff703fa9d02dfd3729c9b6d
Zidi Abderrahmen <zd.abderrahmen@gmail.com>
2026-08-29 14:50:35 +0100

    feat: added role management methods to UserManagementService

--- src/app/core/services/user-management/user-management.service.ts
+++ src/app/core/services/user-management/user-management.service.ts
@@ -4,6 +4,8 @@ import { environment } from '../../../../environments/environment';
 import { Observable } from 'rxjs';
 import { RegisterResponse } from '../../models/user/register/register.response';
 import { RegisterRequest } from '../../models/user/register/register.request';
+import { RoleRequest } from '../../models/user/role/role-request';
+import { RoleResponse } from '../../models/user/role/role-response';
 
 @Service()
 export class UserManagementService {
@@ -42,5 +44,34 @@ export class UserManagementService {
 
     updateUserDeletedStatus(id: string, deleted: boolean): Observable<RegisterResponse> {
         return this.http.patch<RegisterResponse>(`${this.apiUrl}/${id}/deleted/${deleted}`, null, { withCredentials: true });
+    }
+
+    getAllRoles(): Observable<RoleResponse[]> {
+        return this.http.get<RoleResponse[]>(`${this.apiUrl}/roles`, { withCredentials: true });
+    }
+
+    getAllByDeleted(deleted: boolean): Observable<RoleResponse[]> {
+        const endpoint = deleted ? 'deleted' : 'active'; 
+        return this.http.get<RoleResponse[]>(`${this.apiUrl}/roles/${endpoint}`, { withCredentials: true });
+    }
+
+    getRoleById(id: number): Observable<RoleResponse> {
+        return this.http.get<RoleResponse>(`${this.apiUrl}/roles/${id}`, { withCredentials: true });
+    }
+
+    createRole(data: RoleRequest): Observable<RoleResponse> {
+        return this.http.post<RoleResponse>(`${this.apiUrl}/roles`, data, { withCredentials: true });
+    }
+
+    updateRole(id: number, data: RoleRequest): Observable<RoleResponse> {
+        return this.http.put<RoleResponse>(`${this.apiUrl}/roles/${id}`, data, { withCredentials: true });
+    }
+
+    softDeleteRole(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiUrl}/roles/soft/${id}`, { withCredentials: true });
+    }
+
+    hardDeleteRole(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiUrl}/roles/hard/${id}`, { withCredentials: true });
     }
 }
