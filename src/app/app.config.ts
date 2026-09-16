1a99054edd97e476ecd7f8554fa1ae05fad86bd1
Zidi Abderrahmen <zd.abderrahmen@gmail.com>
2026-09-16 16:16:01 +0100

    feat: added cache interceptor to HTTP client and update app initializer logic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,20 @@
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from "@angular/core";
 import { provideRouter } from "@angular/router";
-import { firstValueFrom } from "rxjs";
 import { routes } from "./app.routes";
 import { credentialsInterceptor } from "./core/interceptors/cookies/credentials-interceptor";
 import { errorInterceptor } from "./core/interceptors/error/error-interceptor";
 import { UserService } from "./core/services/user/user.service";
+import { cacheInterceptor } from "./core/interceptors/cache/cache-interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withInterceptors([credentialsInterceptor, errorInterceptor])),
+    provideHttpClient(withInterceptors([credentialsInterceptor, cacheInterceptor, errorInterceptor])),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideAppInitializer(() => {
       const userService = inject(UserService);
-      return firstValueFrom(userService.checkAuth());
-    })
+      userService.checkAuth().subscribe();
+    }),
   ]
 };
